0e0ad844a7936565ca34e5c9411daf1d26dd39f6
Linda MacPhee-Cobb <timestocome@gmail.com>
2017-04-04 13:45:56 -0500

    Added QLearning to RL program
    Not impressed, it does about the same as random guesses but with a wider STD and it's not clear how or why the program buys and sells

--- Reinforcement_tf.py
+++ Reinforcement_tf.py
@@ -20,17 +20,32 @@ import matplotlib.pyplot as plt
 
 
 
+#####################################################################################
+# constants
+# stock, money, shares
+budget = 1000.              # dollars
+n_stocks = 0                # starting shares
+stock = 'MSFT'
+start_date = '2000-01-01'
+end_date = '2017-04-01'
 
+
+
+# network constants
+history = 20             
+n_hidden1 = history
+
+####################################################################################
+# functions
+
+# fetch prices from finance.yahoo.com or save and re-use?
 def get_prices (share_symbol, start_date, end_date, cache_filename='stock_prices.npy'):
 
-    try:
-        stock_prices = np.load(cache_filename)
+    share = Share(share_symbol)
+    stock_hist = share.get_historical(start_date, end_date)
+    stock_prices = [stock_price['Open'] for stock_price in stock_hist]
 
-    except IOError:
-        share = Share(share_symbol)
-        stock_hist = share.get_historical(start_date, end_date)
-        stock_prices = [stock_price['Open'] for stock_price in stock_hist]
-        np.save(cache_filename, stock_prices)
+    print(len(stock_prices ))
 
     return stock_prices
 
@@ -55,6 +70,16 @@ class DecisionPolicy:
 
 
 
+class DecisionPolicy:
+
+    def select_action(self, current_state, step):
+        pass
+
+    def update_Q(self, state, action, reward, next_state):
+        pass 
+
+
+
 
 class RandomDecisionPolicy(DecisionPolicy):
 
@@ -67,6 +92,57 @@ class RandomDecisionPolicy(DecisionPolicy):
 
 
 
+class QLearningDecisionPolicy(DecisionPolicy):
+
+    def __init__(self, actions, n_input):
+        
+        self.epsilon = 0.8      # probability of chosing best action over random action
+        self.gamma = 0.01      # time discount for past data
+        self.actions = actions
+        learning_rate = 0.01
+        n_output = len(actions)
+
+        self.x = tf.placeholder(tf.float32, [None, n_input])
+        self.y = tf.placeholder(tf.float32, [n_output])
+        
+        W1 = tf.Variable(tf.random_normal([n_input, n_hidden1]))
+        b1 = tf.Variable(tf.constant(0.1, shape=[n_hidden1]))
+        h1 = tf.nn.relu(tf.matmul(self.x, W1) + b1)
+
+        W2 = tf.Variable(tf.random_normal([n_hidden1, n_output]))
+        b2 = tf.Variable(tf.constant(0.1, shape=[n_output]))
+        self.q = tf.nn.relu(tf.matmul(h1, W2) + b2)
+
+        loss = tf.square(self.y - self.q)
+        self.train_op = tf.train.AdagradOptimizer(learning_rate).minimize(loss)
+        self.sess = tf.Session()
+        self.sess.run(tf.initialize_all_variables())
+
+
+    def select_action(self, current_state, step):
+
+        threshold = min(self.epsilon, step / 1000.)
+
+        if random.random() < threshold:
+            action_q_vals = self.sess.run(self.q, {self.x: current_state})
+            action_idx = np.argmax(action_q_vals)
+            action = self.actions[action_idx]
+        else:
+            action = self.actions[random.randint(0, len(self.actions) - 1)]
+
+        return action
+
+
+    def update_Q(self, state, action, reward, next_state):
+
+        action_q_vals = self.sess.run(self.q, {self.x: next_state})
+        next_action_q_vals = self.sess.run(self.q, {self.x: next_state})
+        next_action_idx = np.argmax(next_action_q_vals)
+        action_q_vals[0, next_action_idx] = reward + self.gamma * next_action_q_vals[0, next_action_idx]
+        action_q_vals = np.squeeze(np.asarray(action_q_vals))
+        
+        self.sess.run(self.train_op, {self.x: state, self.y: action_q_vals})
+
 
 
 def run_simulation(policy, initial_budget, initial_number_stocks, prices, history, debug=False):
@@ -75,9 +151,8 @@ def run_simulation(policy, initial_budget, initial_number_stocks, prices, histor
     n_stocks = initial_number_stocks
     share_value = 0
     plan = []
-
-
     transitions = list()
+
     for i in range(len(prices) - history - 1):
 
         current_state = np.asmatrix(np.hstack((prices[i:i+history], budget, n_stocks)))
@@ -129,14 +204,16 @@ def run_simulations(policy, budget, n_stocks, prices, history):
 
 
 ##################################################################################################
+# run_simulation
+################################################################################################
+prices = get_prices(stock, start_date, end_date)
 
 actions = ['Buy', 'Sell', 'Hold']
 policy = RandomDecisionPolicy(actions)
 
 budget = 1000.
 n_stocks = 0
-prices = get_prices('MSFT', '1992-01-01', '2017-04-01')
-history = 200
+n_hidden1 = history
 
 avg, std, plans = run_simulations(policy, budget, n_stocks, prices, history)
 
@@ -157,15 +234,23 @@ for p in last_plan:
         ax.scatter(d, price, c='red', alpha=0.7, s=12)
 
 
+# compute how well we did
 years = len(last_plan) / 251
-earnings = (avg - budget) / budget 
-returns = np.power((1 + earnings), (1./years))
+earnings = (avg - budget) / budget * 100.
+net = avg - budget 
+roi = np.power((budget + net/budget), (1./years)) 
 
+
+# feedback to user
 print("Avg earnings", avg)
 print("Std", std)
 print("Years invested", years)
 print("Total earnings %.2lf%%" % (earnings) )
-print("Average earnings ~ %.2lf%%" %(returns) )
+print("Yearly ROI ~ %.2lf%%" %(roi) )
+
+
+
+# plot 
 plt.title("Random policy")
 subtitle = 'Buys are green, Sells are red, avg return on $1000   = %d over %d trading days' % (int(avg), len(last_plan))
 plt.suptitle(subtitle)
