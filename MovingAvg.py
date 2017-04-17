Brute force test buy/sell on moving averages 5-251 trading days fixed shares or fixed $
This method doesn't work. Only bot to make money traded fixed $ using a window >=241 and he didn't make much

MovingAvg.py
@@ -7,10 +7,16 @@
 # this is the first test draft, all I had time to do today
 
 
+# First run, brute force through all moving average windows
+# using either fixed share amounts or fixed dollar amounts
+# buy under, sell over
+# Doesn't work, only one to make money buys on fixed dollar amounts over time > 241 trading days.
+
+
+
 
 
 # todo:
-# try all MA from one day to one year ~250 days
 # try RL bots and see how ML does
 # try GA and see how bots do
 #
@@ -30,12 +36,18 @@ import matplotlib.pyplot as plt
 # select years
 start_year = 1980           # data is 1900-2017 pick year to start training 
 end_year = 2016
-
+#moving_average_days = 251   # length of MA
 
 seed_money = 10000.         # starting cash for bots
 commission = 7.             # flat commission per trade
 fixed_rate = 100.           # fixed dollar bot 
-fixed_share = 1.            # fixed share bot
+fixed_share = 5.            # fixed share bot
+
+
+
+print("Test buying / selling on moving average")
+print("Test fixed dollar purchases against fixed number of shares")
+
 
 ###################################################################################
 # trading bot
@@ -115,67 +127,98 @@ n_samples = len(dja)
 dja['SharePrice'] = dja['DJIA'] / 1000.
 
 
-# calculate moving avg, crossovers and over/under
-dja['MA'] = dja['DJIA'].rolling(window=250).mean()
-dja['OverUnder'] = np.where(dja['DJIA'] > dja['MA'], 1, 0)
-dja['CrossOver'] = np.where(dja['OverUnder'] != dja['OverUnder'].shift(1), 1, 0)
+
+##########################################################################
+# loop over several MA windows
+##########################################################################
+
+smallest_window = 5     # ~1 week
+largest_window = 251    # ~ 1 year
+windows = range(smallest_window, largest_window)
 
 
-# look at data, make sure looks good.
-#print(dja[['Date', 'DJIA','MA', 'OverUnder', 'CrossOver']])
+# for plotting
+fixed_profit = []
+share_profit = []
 
 
-# chose a time frame to run simulations
-dja['year'] = pd.DatetimeIndex(dja.Date).year
-dja = dja[dja.year >= start_year]
-dja = dja[dja.year <= end_year]
+for w in windows:
+    
+    # calculate moving avg, crossovers and over/under
+    dja['MA'] = dja['DJIA'].rolling(window=w).mean()
+    dja['OverUnder'] = np.where(dja['DJIA'] > dja['MA'], 1, 0)
+    dja['CrossOver'] = np.where(dja['OverUnder'] != dja['OverUnder'].shift(1), 1, 0)
 
 
-bot_shares = bot()
-bot_fixed = bot()
+    # look at data, make sure looks good.
+    #print(dja[['Date', 'DJIA','MA', 'OverUnder', 'CrossOver']])
 
 
-# make list of cross over days and split into over/under days
-crossOverDays = dja[dja['CrossOver'] == 1]
-
-for ix, row in crossOverDays.iterrows():
-
-    if row['OverUnder'] == 1:       # buy
-        bot_shares.buy_share(row['SharePrice'])
-        bot_fixed.buy_fixed(row['SharePrice'])
-        print("Buy")
-        print("Bot shares", bot_shares.shares_on_hand, bot_shares.cash_on_hand)
-        print("Bot fixed", bot_fixed.shares_on_hand, bot_fixed.cash_on_hand)
-    else:                           # sell
-        bot_shares.sell_share(row['SharePrice'])
-        bot_fixed.sell_fixed(row['SharePrice'])
-        print("Sell")
-        print("Bot shares", bot_shares.shares_on_hand, bot_shares.cash_on_hand)
-        print("Bot fixed", bot_fixed.shares_on_hand, bot_fixed.cash_on_hand)
+    # chose a time frame to run simulations
+    dja['year'] = pd.DatetimeIndex(dja.Date).year
+    dja = dja[dja.year >= start_year]
+    dja = dja[dja.year <= end_year]
 
 
+    bot_shares = bot()
+    bot_fixed = bot()
 
 
-# cash out
-bot_shares.cash_out(crossOverDays.iloc[-1]['SharePrice'])
-bot_fixed.cash_out(crossOverDays.iloc[-1]['SharePrice'])
+    # make list of cross over days and split into over/under days
+    crossOverDays = dja[dja['CrossOver'] == 1]
 
-print("Final tally:")
-print("Shares bot final $", bot_shares.cash_on_hand )
-print("Fixed bot final $", bot_fixed.cash_on_hand)
+    for ix, row in crossOverDays.iterrows():
 
-
-# print ma days
-print("Cross over days:")
-print(crossOverDays[['Date', 'DJIA', 'SharePrice', 'MA', 'OverUnder']])
+        if row['OverUnder'] == 1:       # buy
+            bot_shares.buy_share(row['SharePrice'])
+            bot_fixed.buy_fixed(row['SharePrice'])
+            #print("Buy")
+            #print("Bot shares", bot_shares.shares_on_hand, bot_shares.cash_on_hand)
+            #print("Bot fixed", bot_fixed.shares_on_hand, bot_fixed.cash_on_hand)
+        else:                           # sell
+            bot_shares.sell_share(row['SharePrice'])
+            bot_fixed.sell_fixed(row['SharePrice'])
+            #print("Sell")
+            #print("Bot shares", bot_shares.shares_on_hand, bot_shares.cash_on_hand)
+            #print("Bot fixed", bot_fixed.shares_on_hand, bot_fixed.cash_on_hand)
 
 
 
+
+    # cash out
+    bot_shares.cash_out(crossOverDays.iloc[-1]['SharePrice'])
+    bot_fixed.cash_out(crossOverDays.iloc[-1]['SharePrice'])
+
+
+    print("*************************************************************")
+    print("Final tally using %d trading days as window:" % w)
+    print("Shares bot final $ %.2lf" % bot_shares.cash_on_hand )
+    print("Fixed $ bot final $ %.2lf" % bot_fixed.cash_on_hand)
+
+
+    # print ma days
+    print("Cross over days:", len(crossOverDays))
+
+
+    # save for plotting
+    fixed_profit.append(bot_fixed.cash_on_hand)
+    share_profit.append(bot_shares.cash_on_hand)
+
+
+
+########################################################################
+# end loop
+#######################################################################
+
+plt.title("Trade on Moving Average, Blue fixed $, Green fixed # shares")
+plt.plot(windows, fixed_profit, c='b')
+plt.plot(windows, share_profit, c='g')
+plt.show()
 
 
 
 
 
 # write to disk
-dja.to_csv("movingAvg_dja.csv")
+#dja.to_csv("movingAvg_dja.csv")
 