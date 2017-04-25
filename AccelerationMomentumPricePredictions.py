84bf2eaed1c38646b5ee0756c1d1f558b8b6ab73
Linda MacPhee-Cobb <timestocome@gmail.com>
2017-04-25 13:37:27 -0500

    streamline code

--- AccelerationMomentumPricePredictions.py
+++ AccelerationMomentumPricePredictions.py
@@ -45,7 +45,7 @@ one_quarter = 63
 
 
 # data to use in training and predictions
-features = ['Volume', 'dx', 'percent_dx', 'd2x',  'dx_abs', 'd2x_abs', 'momentum']
+features = ['Volume', 'dx', 'd2x',  'momentum']
 
 
 
@@ -59,15 +59,16 @@ def read_data(file_name):
 
     
     # want to predict future prices, these are the target values using in training the model
-    stock['next_day'] = stock['Close'].shift(one_day)
     stock['next_week'] = stock['Close'].shift(one_week)
     stock['next_month'] = stock['Close'].shift(one_month)
     stock['next_quarter'] = stock['Close'].shift(one_quarter)
     
 
-    # adjust momentum (Close * dx * dx ) to be in range of other values
-    stock['momentum'] = stock['momentum'] **(1./3.)
-   
+    # calculate velocity, acceleration and momentum
+    stock['dx'] = stock['Close'] - stock['Close'].shift(1)
+    stock['d2x'] = stock['dx'] - stock['dx'].shift(1)
+    stock['momentum'] = ((stock['Close'] * stock['dx'] * stock['dx']) **(1./3.))
+
 
     # split data set into training and holdout
     test_stock = stock.loc[stock.index > '01-01-2017']
@@ -87,14 +88,6 @@ def read_data(file_name):
 
 def train(stock):
 
-    # try one day price prediction 
-    day_model = LinearRegression()
-    train, test, target, test_target = train_test_split(stock[features], stock['next_day'], test_size=0.2)
-    day_model = day_model.fit(train, target)
-    predictions = day_model.predict(test)
-    score = day_model.score(test, test_target)
-    print('Accuracy on next day price:', score)
-    
     
     # one week
     week_model = LinearRegression()
@@ -121,7 +114,7 @@ def train(stock):
     score = quarter_model.score(test, test_target)
     print('Accuracy on next quarter price:', score)
 
-    return day_model, week_model, month_model, quarter_model
+    return week_model, month_model, quarter_model
     
 
 
@@ -135,19 +128,19 @@ print("Training scores:")
 
 print("DJIA")
 dj_stock, dj_train, dj_test = read_data('data/vma_djia.csv')
-dj_day, dj_week, dj_month, dj_quarter = train(dj_train)
+dj_week, dj_month, dj_quarter = train(dj_train)
 
 print("S&P")
 sp_stock, sp_train, sp_test = read_data('data/vma_S&P.csv')
-sp_day, sp_week, sp_month, sp_quarter = train(sp_train)
+sp_week, sp_month, sp_quarter = train(sp_train)
 
 print("Russell")
 r_stock, r_train, r_test = read_data('data/vma_Russell2000.csv')
-r_day, r_week, r_month, r_quarter = train(r_train)
+r_week, r_month, r_quarter = train(r_train)
 
 print("NASDAQ")
 n_stock, n_train, n_test = read_data('data/vma_nasdaq.csv')
-n_day, n_week, n_month, n_quarter = train(n_train)
+n_week, n_month, n_quarter = train(n_train)
 
 
 
@@ -173,16 +166,16 @@ row = dj_stock.index.get_loc('01-09-2017')
 
 
 # Run data through prediction models
-predict_d = dj_day.predict((dj_stock.ix[row + one_day])[features])
 predict_w = dj_week.predict((dj_stock.ix[row + one_week])[features])
 predict_m = dj_month.predict((dj_stock.ix[row + one_month])[features])
 predict_q = dj_quarter.predict((dj_stock.ix[row + one_quarter])[features])
-predictions = [predict_d[0], predict_m[0], predict_q[0], predict_w[0]]
+predictions = [predict_m[0], predict_q[0], predict_w[0]]
 
 
-print("DJIA Actual Jan 9th, 2017: ", dj_actual['Close'])
+print("DJIA Actual Jan 9th, 2017: ", dj_actual['Close'][0])
 print("Std: ", np.std(predictions))
 print("Mean: ", np.mean(predictions))
+print("Diff: %.2lf%%" % (np.mean(predictions - dj_actual['Close'][0]) / dj_actual['Close'][0] * 100.))
 print("Predictions: ", predictions)
 
 
@@ -195,17 +188,17 @@ row = sp_stock.index.get_loc('01-09-2017')
 
 
 # Run data through prediction models
-predict_d = sp_day.predict((sp_stock.ix[row + one_day])[features])
 predict_w = sp_week.predict((sp_stock.ix[row + one_week])[features])
 predict_m = sp_month.predict((sp_stock.ix[row + one_month])[features])
 predict_q = sp_quarter.predict((sp_stock.ix[row + one_quarter])[features])
-predictions = [predict_d[0], predict_m[0], predict_q[0], predict_w[0]]
+predictions = [predict_w[0], predict_m[0], predict_q[0]]
 
 
 
 print("S&P Actual Jan 9th, 2017: ", sp_actual['Close'])
 print("Std: ", np.std(predictions))
 print("Mean: ", np.mean(predictions))
+print("Diff: %.2lf%%" % (np.mean(predictions - sp_actual['Close'][0]) / sp_actual['Close'][0] * 100.))
 print("Predictions: ", predictions)
 
 print("--------------------------------------------")
@@ -216,17 +209,17 @@ row = r_stock.index.get_loc('01-09-2017')
 
 
 # Run data through prediction models
-predict_d = r_day.predict((r_stock.ix[row + one_day])[features])
 predict_w = r_week.predict((r_stock.ix[row + one_week])[features])
 predict_m = r_month.predict((r_stock.ix[row + one_month])[features])
 predict_q = r_quarter.predict((r_stock.ix[row + one_quarter])[features])
 
-predictions = [predict_d[0], predict_m[0], predict_q[0], predict_w[0]]
+predictions = [predict_w[0], predict_m[0], predict_q[0]]
 
 
 print("Russell 2000 Actual Jan 9th, 2017: ", r_actual['Close'])
 print("Std: ", np.std(predictions))
 print("Mean: ", np.mean(predictions))
+print("Diff: %.2lf%%" % (np.mean(predictions - r_actual['Close'][0]) / r_actual['Close'][0] * 100.))
 print("Predictions: ", predictions)
 
 print("--------------------------------------------")
@@ -237,14 +230,14 @@ row = n_stock.index.get_loc('01-09-2017')
 
 
 # Run data through prediction models
-predict_d = n_day.predict((dj_stock.ix[row + one_day])[features])
-predict_w = n_week.predict((dj_stock.ix[row + one_week])[features])
-predict_m = n_month.predict((dj_stock.ix[row + one_month])[features])
-predict_q = n_quarter.predict((dj_stock.ix[row + one_quarter])[features])
+predict_w = n_week.predict((n_stock.ix[row + one_week])[features])
+predict_m = n_month.predict((n_stock.ix[row + one_month])[features])
+predict_q = n_quarter.predict((n_stock.ix[row + one_quarter])[features])
 
-predictions = [predict_d[0], predict_m[0], predict_q[0], predict_w[0]]
+predictions = [predict_w[0], predict_m[0], predict_q[0]]
 
 print("NASDAQ Actual Jan 9th, 2017: ", n_actual['Close'])
 print("Std: ", np.std(predictions))
 print("Mean: ", np.mean(predictions))
+print("Diff: %.2lf%%" % (np.mean(predictions - n_actual['Close'][0]) / n_actual['Close'][0] * 100.))
 print("Predictions: ", predictions)
